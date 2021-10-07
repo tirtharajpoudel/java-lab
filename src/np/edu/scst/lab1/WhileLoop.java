bd5c36cae4d992bbb4effc435e80959c7421ec45
Tirtha Raj Poudel <tirtharajpoudel.trp@gmail.com>
2021-10-07 16:43:04 +0545

    Add while loop

---
+++ src/np/edu/scst/lab1/WhileLoop.java
@@ -0,0 +1,16 @@
+package np.edu.scst.lab1;
+
+/**
+ *
+ * @author Tirtha Raj Poudel
+ */
+public class WhileLoop {
+
+    public static void main(String[] args) {
+	int i = 1;
+	while (i <= 10) {
+	    System.out.println("Tirtha's " + i);
+	    i++;
+	}
+    }
+}

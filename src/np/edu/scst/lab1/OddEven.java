5306e7984c28aedbdedb17ad040fd8ae6a59be63
Tirtha Raj Poudel <tirtharajpoudel.trp@gmail.com>
2021-10-07 16:26:22 +0545

    Add odd even checker in lab 1

---
+++ src/np/edu/scst/lab1/OddEven.java
@@ -0,0 +1,17 @@
+package np.edu.scst.lab1;
+
+/**
+ *
+ * @author Tirtha Raj Poudel
+ */
+public class OddEven {
+
+    public static void main(String[] args) {
+	int a = 108;
+	if (a % 2 == 0) {
+	    System.out.println("Even");
+	} else {
+	    System.out.println("Odd");
+	}
+    }
+}

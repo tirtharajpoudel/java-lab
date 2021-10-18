664421005be2bfe87808492c42d2675b06c81ee7
Tirtha Raj Poudel <tirtharajpoudel.trp@gmail.com>
2021-10-18 20:52:27 +0545

    Add switch case for continuous data

---
+++ src/np/edu/scst/lab2/SwitchCase.java
@@ -0,0 +1,13 @@
+package np.edu.scst.lab2;
+
+/**
+ *
+ * @author Tirtha Raj Poudel
+ */
+public class SwitchCase {
+
+    public static void main(String[] args) {
+	System.out.println("Switch case is used to compare discrete values"
+		+ "but not continuous data.");
+    }
+}

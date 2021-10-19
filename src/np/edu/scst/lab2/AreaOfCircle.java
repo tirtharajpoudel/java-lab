d589933bb155ae42e2ce161fb2661cc88a89f921
Tirtha Raj Poudel <tirtharajpoudel.trp@gmail.com>
2021-10-19 21:50:55 +0545

    Add area of circle

---
+++ src/np/edu/scst/lab2/AreaOfCircle.java
@@ -0,0 +1,17 @@
+package np.edu.scst.lab2;
+
+import java.lang.Math;
+
+/**
+ *
+ * @author Tirtha Raj Poudel
+ */
+public class AreaOfCircle {
+
+    public static void main(String[] args) {
+	double area;
+	int r = 5;
+	area = Math.pow(Math.PI * r, 2);
+	System.out.println("The area of Tirtha's circle is " + area + " sq cm.");
+    }
+}
